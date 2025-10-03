8da3687e69d1f2755a02dd6d4d97e6f15802d18d
Chris Alex <borgesmec7@gmail.com>
2025-10-03 20:45:00 -0300

    Adiciona Variaveis.java mantendo estrutura do Eclipse

---
+++ src/com/loiane/curso/java/aula10/Variaveis.java
@@ -0,0 +1,26 @@
+package com.loiane.curso.java.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+	
+		// Convenção java
+		int idade = 20;
+		String meuNome = "Chris";
+		String nomeDaMinhaNamorada = "Marcella";
+		
+		// Não são convenções java
+		String MeuNome;
+		String NomeDaMinhaNamorada;
+		
+		// na prática
+		idade = 22;
+		String a = "Fluminense";
+		
+		
+		
+		
+		System.out.println("Meu nome é " + meuNome + ", tenho " + idade + " anos e minha namorada é a " + nomeDaMinhaNamorada);
+	}
+
+}
