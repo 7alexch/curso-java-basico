5fb820552caccb496c33d1f0da554dd06e9f4ab7
Chris Alex <borgesmec7@gmail.com>
2025-10-04 18:09:56 -0300

    Aula sobre variaveis e tipos primitivos.

---
+++ src/com/loiane/curso/java/aula11/VairaveisChar.java
@@ -0,0 +1,35 @@
+package com.loiane.curso.java.aula11;
+
+public class VairaveisChar {
+
+	public static void main(String[] args) {
+		
+		char o = 'o';
+		
+		char i = 'i';
+		
+		// retorna 216, tabela ASCII
+		
+		System.out.println(o+i);
+		
+		// retorna os valores das variáveis 
+		
+		System.out.println(""+o+i);
+		
+		o = 111;
+		
+		i = 105;
+		
+
+		// retorna 216 da soma dos valores
+		
+		System.out.println(o+i);
+		
+		// retorna o nome das variaveis juntas
+		
+		System.out.println(""+o+i);
+		
+		
+	}
+
+}
---
+++ src/com/loiane/curso/java/aula11/VariaveisInteirasFlutuantes.java
@@ -0,0 +1,20 @@
+package com.loiane.curso.java.aula11;
+
+public class VariaveisInteirasFlutuantes {
+
+	public static void main(String[] args) {
+		
+		// muito utilizado na linguagem java
+		double valorDaPassagemSG = 3.95;
+		
+		// não tão utilizado pois precisa por "f" no fim, um literal
+		
+		float valorDaPassagemNiteroi = 5.60f;
+		
+		System.out.println("Valor da passagem de São Gonçalo é: " + valorDaPassagemSG);
+		
+		System.out.println("Valor da passagem de Niterói é: " + valorDaPassagemNiteroi);
+		
+	}
+
+}
