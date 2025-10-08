d2232756446142dc22976d71d3b0bf94223730da
Chris Alex <borgesmec7@gmail.com>
2025-10-08 19:57:19 -0300

    Aula 16 - Loop for

---
+++ src/com/loiane/curso/java/aula16/LoopFor.java
@@ -0,0 +1,21 @@
+ package com.loiane.curso.java.aula16;
+
+public class LoopFor {
+
+	public static void main(String[] args) {
+		
+	// inicialização / expressão booleana / atualização  | vai contar 5 vezes a partir de 0, exceto se eu por "<="
+		for (int i = 0; i < 5; i++) {
+			
+			System.out.println("O valor de i é: " + i);
+			
+		}
+		for (int i = 5; i > 0; i--) {
+			
+			System.out.println("O valor de i é: " + i);
+			
+		}
+
+	}
+
+}
