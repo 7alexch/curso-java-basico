85d0a68712f2a8c1c0686cfa0a6ba6aae0214d98
Chris Alex <borgesmec7@gmail.com>
2025-10-10 19:30:53 -0300

    aula 19, vetores ainda incompleta

---
+++ src/com/loiane/curso/java/aula19/Arrays01.java
@@ -0,0 +1,23 @@
+package com.loiane.curso.java.aula19;
+
+
+public class Arrays01 {
+		// igual o Strng[] significar que é um array de strings, pode passar mais de um argumento
+	public static void main(String[] args) {
+	
+		
+		// criando um array:
+		
+// tipo double | nome do array | criando o array e colocando quantos indices vai ter.
+		double[] temperaturas = new double[365];
+		temperaturas[0] = 20.5;
+		temperaturas[1] = 22.2;
+		temperaturas[2] = 22;
+		temperaturas[3] = 25;
+		temperaturas[4] = 29;
+		
+		System.out.println("a temperatura do dia 1 é: " + temperaturas[0]);
+		
+		// PAREI A AULA NO MINUTO 10:35
+	}
+}
