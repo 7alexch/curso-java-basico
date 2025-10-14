608ea2762804a2da1dd76e8f11370a3a397a35aa
Chris Alex <borgesmec7@gmail.com>
2025-10-14 17:01:36 -0300

    aula 20 matrizes e exercicio 01 completo

--- src/com/loiane/curso/java/aula20/Exercicio01.java
+++ src/com/loiane/curso/java/aula20/Exercicio01.java
@@ -1,21 +1,38 @@
 package com.loiane.curso.java.aula20;
 
+import java.util.Random;
+
 public class Exercicio01 {
 
 	public static void main(String[] args) {
 		
-		int [][] numerosAleatorios = new int[4][4];
+		int[][] numerosAleatorios = new int[4][4];
 		
-		random numeroRandom = new random ();
+		Random numeroRandom = new Random();
 		
-		for (int i = 0; i< numerosAleatorios.length; i++) {
-			for(int j=0;j<numerosAleatorios[i].length; j++) {
-			
-				numerosAleatorios[i][j] = numeroRandom.nextInt();
-	
+		for(int i = 0;i<numerosAleatorios.length;i++) {
+			for(int j = 0; j<numerosAleatorios[i].length;j++) {
+				numerosAleatorios[i][j] = numeroRandom.nextInt(100);
 			}
 		}
-
+		
+		int maior = 0;
+		int linha = 0;
+		int col = 0;
+		for(int i = 0;i<numerosAleatorios.length;i++) {
+			for(int j = 0; j<numerosAleatorios[i].length;j++) {
+				numerosAleatorios[i][j] = numeroRandom.nextInt(100);
+				if (numerosAleatorios[i][j] > maior) {
+					maior = numerosAleatorios[i][j];
+					linha = i;
+					col = j;
+				}
+			}
+		}
+		System.out.println("O maior valor da Matriz é: " + maior);
+		System.out.println("Linha: " + linha);
+		System.out.println("Coluna: " + col);
+		
 	}
 
 }
