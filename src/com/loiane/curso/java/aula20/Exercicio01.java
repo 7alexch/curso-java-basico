34500e90b83a5e45a10bb6402a94e367d251aa04
Chris Alex <borgesmec7@gmail.com>
2025-10-14 17:27:09 -0300

    aula 20 exercicios completos

--- src/com/loiane/curso/java/aula20/Exercicio01.java
+++ src/com/loiane/curso/java/aula20/Exercicio01.java
@@ -29,6 +29,15 @@ public class Exercicio01 {
 				}
 			}
 		}
+		
+		for(int i = 0;i<numerosAleatorios.length;i++) {
+			for(int j = 0; j<numerosAleatorios[i].length;j++) {
+				
+				System.out.print(numerosAleatorios[i][j] + " ");
+
+			}
+			System.out.println();
+		}
 		System.out.println("O maior valor da Matriz é: " + maior);
 		System.out.println("Linha: " + linha);
 		System.out.println("Coluna: " + col);
